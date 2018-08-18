5054e875df383a569adbdc4548e09c23e34dd47f
Stephen Pellegrini <spellegrini156@gmail.com>
2018-08-18 11:36:39 -0700

    separtate route file for html routes

--- express.js
+++ express.js
@@ -8,24 +8,16 @@ var port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+var tableArray = [];
 
-//routes
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
 
-app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "tables.html"));
-});
 
-app.get("/Make", function(req, res) {
-    res.sendFile(path.join(__dirname, "Make.html"));
-});
 
 //api routes
 app.post("/api/tables", function(req, res) {
     var newReservation = req.body;
     newReservation.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+    tableArray.push(newReservation);
 });
 
 //start the app
