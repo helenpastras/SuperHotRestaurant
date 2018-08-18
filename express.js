ad889c017d74b55c6d12cafcf0b03430aaead92e
Eleni Pastra <hell-en@Elenis-MacBook-Pro.local>
2018-08-18 11:59:03 -0700

    added variables for resies and waitlist

--- express.js
+++ express.js
@@ -8,9 +8,38 @@ var port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var tableArray = [];
-
-
+var reservations = [
+    {
+    routeName: "lars",
+    name: "Lars Ulrich",
+    phone: 1800-"Drumset",
+    email: "drums@metallica.com",
+    uniqueID:"001200"
+    },
+    {
+    routeName: "dude",
+    name: "The Dude",
+    phone: 818-818-8888,
+    email: "whiterussiansforlife@dude.com",
+    uniqueID:"0011110"
+    },
+];
+var waitlist = [
+    {
+    routeName: "others",
+    name: "Not cool people",
+    phone: 1234567890,
+    email: "me@me.com",
+    uniqueID:"102000"
+    },
+    {
+    routeName: "none",
+    name: "One person",
+    phone: 1234561234,
+    email: "no@non.com",
+    uniqueID:"1234"
+    },
+];
 
 
 //api routes
